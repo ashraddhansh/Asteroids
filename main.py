47825cc5d1e0a84f224e2f55758fe4538bc194b0
ashraddhansh <ashraddhansh@protonmail.com>
2025-07-18 19:34:33 +0530

    add code for 60fps

--- main.py
+++ main.py
@@ -4,13 +4,16 @@ from constants import *
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
+    dt = 0
+    clock = pygame.time.Clock()
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-            pygame.Surface.fill(screen, (0,0,0))
+            screen.fill("black")
             pygame.display.flip()
+        dt = clock.tick(60)/1000
 
 if __name__ == "__main__":
     main()
