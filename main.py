35aa5c7140e334648d840341a05d2b10a60c0b54
ashraddhansh <ashraddhansh@protonmail.com>
2025-07-19 13:15:42 +0530

    add object distruction code

--- main.py
+++ main.py
@@ -40,7 +40,6 @@ def main():
                 print("Game Over!")
                 sys.exit()
 
-        for asteroid in asteroids:
             for shot in shots:
                 if asteroid.checkcollisions(shot):
                     asteroid.kill()
