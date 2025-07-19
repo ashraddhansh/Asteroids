18d2a7954136ac7e139027a268825f106748d300
ashraddhansh <ashraddhansh@protonmail.com>
2025-07-19 13:10:01 +0530

    add rate limit in shooting

--- player.py
+++ player.py
@@ -39,14 +39,17 @@ class Player(CircleShape):
         if keys[pygame.K_s]:
             self.move(-dt)
 
-        if keys[pygame.K_SPACE]:
-            self.shoot(self)
+        if keys[pygame.K_SPACE] and self.timer <= 0:
+            self.shoot()
+
+        if self.timer > 0:
+            self.timer -= dt
 
     def move(self,dt):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
         self.position += forward * PLAYER_SPEED * dt
 
-    def shoot(self,player):
-        new_shot = Shot(player.position.x,player.position.y)
+    def shoot(self):
+        new_shot = Shot(self.position.x, self.position.y)
         new_shot.velocity = pygame.Vector2(0,1).rotate(self.rotation)*PLAYER_SHOOT_SPEED
         self.timer = PLAYER_SHOOT_COOLDOWN
